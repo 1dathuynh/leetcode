57e550c33788bcb614f7e92dd85880cbe42d3c3b
1dathuynh <dathuynh1221212@gmail.com>
2024-07-09 16:26:54 +0700

    Deleted index.html and modified longestConsecutive.js

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -1,28 +1,53 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var longestConsecutive = function(nums) {
+//     set = new Set(nums)
+//     nums = [...set]
+//     if(nums.length === 0){
+//       return 0
+//     }
+//       nums.sort((a,b) => a - b)
+//       i = 0
+//       len = 1
+//       let res = []
+//       while(i < nums.length){
+//         if(nums[i + 1] - nums[i] === 1){
+//           len++
+//         }
+//         else{
+//           res.push(len)
+//           len = 1
+//         }
+//         ++i
+//       }
+//       return Math.max(...res)
+
+//   }
+// Time : O(nlogn)
+// Space : O(n)
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    set = new Set(nums);
-    nums = [...set];
-    if(nums.length === 0){
-      return 0;
-    }
-      nums.sort((a,b) => a - b);
-      i = 0;
-      len = 1;
-      let res = [];
-      while(i < nums.length){
-        if(nums[i + 1] - nums[i] === 1){
-          len++;
-        }
-        else{
-          res.push(len);
-          len = 1;
-        }
-        ++i;
+
+  set = new Set(nums);
+  longest = 0;
+  if(nums.length == 0){
+    return longest;
+  }
+  for(num of set){
+    if(!set.has(num - 1)){
+      length = 1;
+      while(set.has(num + length)){
+        length++;
       }
-      return Math.max(...res)
-  
-  };
-  
+      longest = max(length, longest);
+    }
+  }
+  return longest;
+};
+
