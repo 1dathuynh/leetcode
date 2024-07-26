f95610cd682a9953854fc064b675b923deb9ace0
1dathuynh <dathuynh1221212@gmail.com>
2024-07-26 15:47:49 +0700

    MinimumWindownSubstring

---
+++ 76.MinimunWindowSubstring.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    let countT = new Map();
+    let window = new Map();
+    let [l, r] = [0, 0];
+    for(let s of t){
+        countT.set(s, (countT.get(s) || 0) + 1);
+    }
+    let [have, need] = [0, countT.size];
+    res = [-1, -1]; minLen = Number.MAX_VALUE;
+    while(r !== s.length){
+        char = s[r];
+        window.set(char, (window.get(char) || 0) + 1);
+        if(countT.has(char) && window.get(char) === countT.get(char)){
+            have++;
+        }
+        while(have === need){
+            if((r - l + 1) < minLen){
+                res = [l, r];
+            }
+            window.get(s[l], window.get(s[l]) - 1);
+            if(countT.has(s[l]) && window.get(s[l]) < countT.get(s[l])){
+                have--;
+            }
+            l++;
+        }
+        r++;
+    }
+    [l, r] = res;
+    return (minLen ===Number.MAX_VALUE) ? "" : s.slice[l, r + 1] 
+};
