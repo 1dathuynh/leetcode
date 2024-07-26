b26523051a419847395e07d69719ee77c634cac7
1dathuynh <dathuynh1221212@gmail.com>
2024-07-26 20:34:39 +0700

    MinimumWindowSubstring

--- 76.MinimumWindowSubstring.js
+++ 76.MinimumWindowSubstring.js
@@ -10,20 +10,22 @@ var minWindow = function(s, t) {
     for(let s of t){
         countT.set(s, (countT.get(s) || 0) + 1);
     }
+    
     let [have, need] = [0, countT.size];
     res = [-1, -1]; minLen = Number.MAX_VALUE;
     while(r !== s.length){
         char = s[r];
         window.set(char, (window.get(char) || 0) + 1);
-        if(countT.has(char) && window.get(char) === countT.get(char)){
+        if(countT.has(char) && (window.get(char) === countT.get(char))){
             have++;
         }
         while(have === need){
             if((r - l + 1) < minLen){
                 res = [l, r];
+                minLen = (r - l + 1);
             }
-            window.get(s[l], window.get(s[l]) - 1);
-            if(countT.has(s[l]) && window.get(s[l]) < countT.get(s[l])){
+            window.set(s[l], window.get(s[l]) - 1);
+            if(countT.has(s[l]) && (window.get(s[l]) < countT.get(s[l]))){
                 have--;
             }
             l++;
@@ -31,5 +33,6 @@ var minWindow = function(s, t) {
         r++;
     }
     [l, r] = res;
-    return (minLen ===Number.MAX_VALUE) ? "" : s.slice[l, r + 1] 
+    return (minLen ===Number.MAX_VALUE) ? "" : s.slice(l, r + 1) 
 };
+
